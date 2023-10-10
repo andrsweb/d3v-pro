b1758f74060f2fb947f5ede3a98f5a5fa7947df7
andrsweb <proflinervk@gmail.com>
2023-10-10 16:01:05 +0700

    change Button component

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,10 +1,11 @@
 import styles from './Button.module.css'
+import { Link } from 'react-router-dom'
 
 const Button = ({ text, onClick }) => {
 	return (
-		<button className={styles.button} data-text={text} onClick={onClick}>
+		<Link to='/main' className={styles.button} data-text={text} onClick={onClick}>
 			{text}
-		</button>
+		</Link>
 	)
 }
 
