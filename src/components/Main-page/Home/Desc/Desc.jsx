9d275f8eb1e8da90310e74c20337a45a0bbd075e
andrsweb <proflinervk@gmail.com>
2023-10-16 01:38:38 +0700

    add cat img

---
+++ src/components/Main-page/Home/Desc/Desc.jsx
@@ -0,0 +1,15 @@
+import styles from './Desc.module.css'
+
+const Desc = () => {
+	return (
+		<section className={styles.desc}>
+			<div className="container">
+				<div className={styles.descWrapper}>
+					<h2 className='h2'>
+						
+					</h2>
+				</div>
+			</div>
+		</section>
+	)
+}
